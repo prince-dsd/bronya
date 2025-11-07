ddfd16497ef295f97b64652529edd60044e08d54
Prince <neo11prince@gmail.com>
2025-11-07 18:43:43 +0530

    Modify script

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ poetry install --no-interaction --no-ansi
 pm2 start pm2.config.js
 
 # Save the PM2 process list
-#pm2 save
+pm2 save
 
 # Generate and configure the PM2 startup script
 # pm2 startup | tail -n 1 | xargs sudo
 
 # Display the status of PM2 processes
-# pm2 status
+pm2 status
