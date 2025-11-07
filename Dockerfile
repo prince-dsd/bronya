24f4abcfca3e81d670f4a5dd2a1be4e51f0b88ea
Prince <neo11prince@gmail.com>
2025-11-07 17:29:35 +0530

    Modify setup and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,9 @@ SHELL ["/bin/bash", "-l", "-c"]
 ARG TZ=UTC
 ENV TZ=${TZ}
 
+WORKDIR /var/app
+COPY . .
+
 COPY setup.sh /usr/local/bin/
 RUN set -eu && chmod +x /usr/local/bin/setup.sh
 
@@ -18,13 +21,17 @@ RUN set -eu && \
     export NVM_DIR="$HOME/.nvm" && \
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" && \
-    nvm install 13.6.0 && \
+    nvm install 24.11.0 && \
     npm install -g pm2
+    
 # Set environment variables for Poetry
 ENV POETRY_VIRTUALENVS_CREATE=false \
     POETRY_NO_INTERACTION=1
 
 RUN /usr/local/bin/python -m pip install --upgrade pip && \
 /usr/local/bin/python -m pip install --no-cache-dir poetry
+
+# Expose optional API port for PM2 dashboard
+EXPOSE 9615
 
 ENTRYPOINT ["/bin/bash", "-l", "-c", "setup.sh"]
--- setup.sh
+++ setup.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 set -eu
-
-# Navigate to the application directory
-cd /var/app
-
 # Install dependencies using Poetry
-poetry install --no-venv
+poetry install --no-interaction --no-ansi
+# Activate the Poetry virtual environment
+# poetry shell
 
-# # Activate the Poetry virtual environment
-# # poetry shell
+# Run database migrations
+# poetry run alembic upgrade head
 
-# # Run database migrations
-poetry run alembic upgrade head
+# Start PM2 processes defined in the configuration file
+pm2 start pm2.config.js
 
-# # Start PM2 processes defined in the configuration file
-# pm2 start pm2.config.js
+# Save the PM2 process list
+#pm2 save
 
-# # Save the PM2 process list
-# pm2 save
-
-# # Generate and configure the PM2 startup script
+# Generate and configure the PM2 startup script
 # pm2 startup | tail -n 1 | xargs sudo
 
-# # Display the status of PM2 processes
+# Display the status of PM2 processes
 # pm2 status
