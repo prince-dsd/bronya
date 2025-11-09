0c0d56d943a0fff06d83f72b23c49f0f3c664aff
Prince <neo11prince@gmail.com>
2025-11-09 14:40:39 +0530

    Setup

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 set -eu
-# Install dependencies using Poetry
-poetry install --no-interaction --no-ansi
-# Activate the Poetry virtual environment
 # poetry shell
 
 # Run database migrations
 # poetry run alembic upgrade head
 
 # Start PM2 processes defined in the configuration file
-pm2 start pm2.config.js
+pm2-runtime pm2.config.js
 
 # Save the PM2 process list
-pm2 save
+#pm2 save
 
 # Generate and configure the PM2 startup script
 # pm2 startup | tail -n 1 | xargs sudo
 
 # Display the status of PM2 processes
-pm2 status
+#pm2 status
